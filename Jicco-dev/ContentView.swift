f46cc0e04bdc977a3ff5271265f32c0aed8312f9
dxk19911212 <iishoni@163.com>
2022-03-02 01:00:40 +0800

    Initial Commit

---
+++ Jicco-dev/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Jicco-dev
+//
+//  Created by admin on 2022/3/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jicco-dev/Jicco_devApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jicco_devApp.swift
+//  Jicco-dev
+//
+//  Created by admin on 2022/3/2.
+//
+
+import SwiftUI
+
+@main
+struct Jicco_devApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
